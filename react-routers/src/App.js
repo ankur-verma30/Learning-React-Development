84873f9ec5a7b80fae23bce2ad4892e4a25a502b
Ankur Verma <ankurverma7619@gmail.com>
2024-07-26 23:39:34 +0530

    Learning React Routes

---
+++ react-routers/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import './App.css';
+import  {Navbar}  from './Components/Navbar';
+import { OrderSummary } from './Components/OrderSummary';
+import NoMatch from './Components/NoMatch';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='order-summary' element={<OrderSummary/>} />
+      <Route path ='*' element={<NoMatch />} />
+
+    </Routes>
+    </>
+  );
+}
+
+export default App;
