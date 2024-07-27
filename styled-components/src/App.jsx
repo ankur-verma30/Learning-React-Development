a91f46d3407432069706f7c4c40983d4a6718120
Ankur Verma <ankurverma7619@gmail.com>
2024-07-27 22:41:21 +0530

    Started Learning Styled Components

---
+++ styled-components/src/App.jsx
@@ -0,0 +1,24 @@
+import StyledButton from './Components/Button'
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className='App'>
+     <StyledButton>Submit Button</StyledButton>
+     <div>
+      <br />
+     </div>
+     <StyledButton variant='outline'>Outlined Styled Button</StyledButton>
+     </div>
+    </>
+
+    
+  )
+}
+
+export default App
---
+++ styled-components/src/Components/Button.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+	border: 2px solid #4caf50;
+	background-color: ${(props)=> props.variant==='outline' ? 'red' : 'blue'};
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	cursor: pointer;
+	transition: 0.5s all ease-out;
+`;
+export default StyledButton
