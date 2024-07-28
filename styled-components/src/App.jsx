a0757aba6ebda468bdc261cc54a60185b585b81c
Ankur Verma <ankurverma7619@gmail.com>
2024-07-28 11:09:34 +0530

    styled Components Completed

--- styled-components/src/App.jsx
+++ styled-components/src/App.jsx
@@ -1,24 +1,51 @@
-import StyledButton from './Components/Button'
-import './App.css'
+import { StyledButton, FancyButton,SubmitButton,AnimateLogo,DarkButton } from "./Components/Button";
+import { ThemeProvider,createGlobalStyle } from "styled-components";
+import "./App.css";
+import logo from '/vite.svg'
 
+const theme={
+  dark:{
+    primary:'000',
+    text:'#fff',
+  },
+  light:{
+    primary:'#fff',
+    text:'#000',
+  }
+  ,
+  fontFamily:'Segoe UI',
+}
 
+const GlobalStyles=createGlobalStyle`
+  button{
+    font-family: ${(props)=>props.theme.fontFamily};
+  }
+`
 
 function App() {
 
-
-  return (
-    <>
-    <div className='App'>
-     <StyledButton>Submit Button</StyledButton>
-     <div>
-      <br />
-     </div>
-     <StyledButton variant='outline'>Outlined Styled Button</StyledButton>
-     </div>
-    </>
-
-    
-  )
+	return (
+    <ThemeProvider theme={theme} >
+      <GlobalStyles/>
+		<>
+			<div className="App">
+        <AnimateLogo src={logo}/>
+				<StyledButton>Submit Button</StyledButton>
+        <StyledButton type="submit">finish Task </StyledButton>
+				<div>
+					<br />
+				</div>
+				<StyledButton variant="outline">Outlined Styled Button</StyledButton>
+				<FancyButton as={'a'}>Fancy Button</FancyButton>
+        <div>
+					<br />
+				</div>
+        <SubmitButton>Attibute Submit</SubmitButton>
+        <DarkButton>Dark Theme</DarkButton>
+			</div>
+		</>
+    </ThemeProvider>
+	);
 }
 
-export default App
+export default App;
--- styled-components/src/Components/Button.jsx
+++ styled-components/src/Components/Button.jsx
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 
-const StyledButton = styled.button`
+export const StyledButton = styled.button`
 	border: 2px solid #4caf50;
-	background-color: ${(props)=> props.variant==='outline' ? 'red' : 'blue'};
+	background-color: ${(props) =>
+		props.variant === "outline" ? "red" : "blue"};
 	color: white;
 	padding: 15px 32px;
 	text-align: center;
@@ -11,5 +12,40 @@ const StyledButton = styled.button`
 	font-size: 16px;
 	cursor: pointer;
 	transition: 0.5s all ease-out;
+	&:hover{
+		background-color: ${(props) =>
+		props.variant !== "outline" ? "red" : "blue"};
+	}
 `;
-export default StyledButton
+
+export const FancyButton = styled(StyledButton)`
+	background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
+	border: none;
+`;
+
+
+export const SubmitButton = styled(StyledButton).attrs({type: 'submit'})`
+	background-color: yellow;
+	border-radius: 5px;
+	text-transform: capitalize;
+`
+const Rotate=keyframes`
+	from{
+		transform: rotate(0deg);
+	}
+	to{
+		transform: rotate(360deg);
+	}
+`
+
+export const AnimateLogo=styled.img`
+	height: 40vmin;
+	pointer-events: none;
+	animation:${Rotate} infinite 5s linear;
+`
+
+export const DarkButton=styled(StyledButton)`
+border:2px solid ${props=>props.theme.dark.primary};
+background-color: ${props=>props.theme.dark.primary};
+color: ${props=>props.theme.dark.text};
+`
